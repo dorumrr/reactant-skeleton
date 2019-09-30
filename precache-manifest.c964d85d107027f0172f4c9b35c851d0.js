f4af3cbb1baeb8f9320aa5f053213d78e5ee2ab2
Doru Moraru <doru.moraru@gmail.com>
2019-09-30 22:45:19 +0100

    Updates

---
+++ precache-manifest.c964d85d107027f0172f4c9b35c851d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cc506c73d1701ed26332d89c9c12ef",
+    "url": "/reactant-skeleton/index.html"
+  },
+  {
+    "revision": "352f7627088edec36d13",
+    "url": "/reactant-skeleton/static/css/2.23f153e0.chunk.css"
+  },
+  {
+    "revision": "4a93a11b0b36fbe1d75f",
+    "url": "/reactant-skeleton/static/css/main.f0cf56cc.chunk.css"
+  },
+  {
+    "revision": "352f7627088edec36d13",
+    "url": "/reactant-skeleton/static/js/2.9df9d425.chunk.js"
+  },
+  {
+    "revision": "4a93a11b0b36fbe1d75f",
+    "url": "/reactant-skeleton/static/js/main.a885c442.chunk.js"
+  },
+  {
+    "revision": "5e84504399930238a4e7",
+    "url": "/reactant-skeleton/static/js/runtime-main.d6d40870.js"
+  }
+]);
